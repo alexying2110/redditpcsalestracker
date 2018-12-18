3e9840f9c933eb3f9f5b590f9ef7eecd9d31c460
lofatdairy <alexy237@gmail.com>
2018-12-18 16:26:56 -0500

    added client secret and key variables to make it more obvious where they go for joseph

--- saletracker.py
+++ saletracker.py
@@ -1,8 +1,10 @@
 import praw
-import win32api
+from win10toast import ToastNotifier
 
 
 def main():
+    notification = ToastNotifier()
+    
     keyWords = []
     keyWord = input("Enter keyword (type q to stop): ")
 
@@ -16,29 +18,20 @@ def main():
     #praw OAuth
     bot = praw.Reddit(user_agent="salestracker",
                       username=userName,
-                      password=passWord)
+                      password=passWord,
+                      client_id = "",
+                      client_secret = "")
 
     sub = bot.subreddit('buildapcsales')
-
+    
     print(keyWords)
     #set skip_existing to True to only notify user about new posts
     for submission in sub.stream.submissions(skip_existing=False):
         title = submission.title
         for key in keyWords:
             if key.lower() in title.lower():
-                notify_user(title)
-
-#plays a stupid little tune, there are probably better ways to do this but i already had this package
-#installed anyways
-def notify_user(title):
-    win32api.Beep(440, 250)
-    win32api.Beep(880, 125)
-    win32api.Beep(440, 125)
-    win32api.Beep(880, 125)
-    win32api.Beep(440, 125)
-    win32api.Beep(220, 250)
-    print(title)
-
+                print(title)
+                notification.show_toast(title, key.lower())
 
 if __name__ == '__main__':
     main()
